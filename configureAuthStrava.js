96560b465018fd815f41d9d2a8d60d6cb1585fce
Laura <l.maya10@uniandes.edu.co>
2019-12-02 22:07:37 -0500

    Funciona log in bien

--- configureAuthStrava.js
+++ configureAuthStrava.js
@@ -15,7 +15,7 @@ const configureAuthStrava = app => {
       {
         clientID: STRAVA_CLIENT_ID,
 			  clientSecret: STRAVA_CLIENT_SECRET,
-        callbackURL: "https://rutasbici.herokuapp.com/auth/strava/callback"
+        callbackURL: "auth/strava/callback"
 		  },
 		  (accessToken, refreshToken, profile, done) =>{
 			  // asynchronous verification, for effect...
