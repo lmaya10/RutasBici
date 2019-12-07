16e2de9b22ca6a3f5ba6de71b8caeb02395cb2b7
Laura <l.maya10@uniandes.edu.co>
2019-12-06 20:46:20 -0500

    Conectado a mongo

--- front/src/Componentes/BuscarRutas.js
+++ front/src/Componentes/BuscarRutas.js
@@ -1,15 +1,40 @@
 import React, {useEffect, useState} from 'react';
 function BuscarRutas(props) {
+  const [paseos, setPaseos] = useState([]);
+  const [err, setErr] = useState("");
+
+	useEffect(() => {
+		fetch("paseos")
+      .then(res => res.json())
+      .then( data => {
+        if(data.err) {
+          setErr(JSON.stringify(data.msg));
+        }
+        else{
+          setPaseos(data);
+        }
+      });
+	}, [])
 
 
-
-
-  return (
-    <div>
-      <h1>Buscar Rutas</h1>
-
-    </div>
-    );
+  const renderPaseos = () => paseos.map(d => 
+  	<div>
+			<h1>{d.nombre}</h1>
+			<br/>
+  	</div>
+  )
+	
+  return(
+      <div>
+        <div >
+          <h1 className = "tituloGrupos">Rutas Actuales</h1>
+          <br/>
+        </div>
+        <div class="ListaPaseos">
+        	{renderPaseos()}
+        </div>
+      </div>
+    )
 }
 
 export default BuscarRutas;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require("express");
 var router = express.Router();
+const path=require("path");
+const app = express();
+
+const MyMongoLib = require("../MyMongoLib");
+const myMongoLib = MyMongoLib();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" });
 });
+
+router.post("/crearPaseo", function(req,res,next) {
+
+  console.log("crearProducto", req.body);
+  crearPaseo(req.body);
+  res.redirect("/grupos");
+
+});
+
+
+function crearPaseo(content, callback){
+  console.log("Entra a crearPaseo");
+  myMongoLib.crearPaseo(content);
+
+}
+
+router.get("/paseos", (req, res) => {
+  console.log("Get Paseos");
+  myMongoLib.getPaseos()
+    .then(docs => res.send(docs))
+    .catch(err => res.send({err: true, msg: err}));
+});
+
 
 module.exports = router;
