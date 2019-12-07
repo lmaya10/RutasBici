16befbc0ab7b51934cbddef63bee6081e02d043d
Francisco Mcallister Ortiz <fj.mac@uniandes.edu.co>
2019-12-06 21:22:39 -0500

    home

--- front/src/Componentes/Home.js
+++ front/src/Componentes/Home.js
@@ -3,9 +3,11 @@ import MisRutas from "./MisRutas.js";
 import Mapa from "./Mapa.js";
 
 function Home(props) {
+  const[ub,setUb]=useState([]);
 
+  useEffect(() => {
 
-
+  }, [])
 
   return (
     <div>
