3a826d3bd433752c362f930bff9a900c6778b725
Laura <l.maya10@uniandes.edu.co>
2019-12-09 19:45:20 -0500

    Rutas

--- routes/index.js
+++ routes/index.js
@@ -42,9 +42,9 @@ router.get("/paseos", (req, res) => {
 });
 
 
-//router.get("/*", (req, res) => {
-//  res.sendFile(path.join(__dirname, "../front/build/index.html"));
-//});
+router.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../front/build/index.html"));
+});
 
 
 //app.get("/*", (req, res) => {
